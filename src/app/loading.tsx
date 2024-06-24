bd4a47f18e2463c7dd0a12dbc3aefa4797e1c89b
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-06-24 11:05:30 +0600

    loading image added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,10 @@
+import loadingImage from '@/assest/loading/loading.gif'
+import Image from 'next/image'
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <Image width={100} height={100} src={loadingImage} alt='loadingImage'/>
+    </div>
   )
 }
 
